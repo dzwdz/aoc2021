b9843d07a45e6ae85505755bd2dc1238e45b5e11
dzwdz <kg67199@gmail.com>
2021-12-03 11:41:43 +0100

    clean up 3.1, add space to solve 3.2

--- 3.hs
+++ 3.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Ord
 
 readBase2 :: String -> Integer
 readBase2 = loop . reverse where
@@ -7,25 +8,33 @@ readBase2 = loop . reverse where
   loop  ""       = 0
 
 rotate :: [[a]] -> [[a]]
+rotate [] = []
 rotate [a] = map (\x->[x]) a
 rotate (x:xs) = concat2 (rotate [x]) (rotate xs)
   where
-    concat2 a b = map (\(a,b)->a++b) $ zip a b -- TODO pointless
+    concat2 a b = map (uncurry (++)) $ zip a b -- TODO why can't i make this pointless?
 
+uniq :: Eq a => [a] -> [a]
 uniq (x:xs) = [x] ++ (filter (x /=) $ uniq xs)
 uniq [] = []
 
 -- creates a list sorted from least to most frequent
 mostFrequent :: Eq a => [a] -> [a]
-mostFrequent list = sortBy (\a b->compare (count a) (count b)) $ uniq list
+mostFrequent list = sortBy (comparing count) $ uniq list
   where
     count el = length $ filter (\x -> x == el) list
 
-sol input = gamma * epsilon
+
+part1 input = gamma * epsilon
   where
     freqs   = map mostFrequent $ rotate input
     gamma   = readBase2 $ map last freqs
     epsilon = readBase2 $ map head freqs
 
+part2 input = "TODO"
+
+
 main :: IO ()
-main = interact $ show . sol . lines
+main = interact $ wrapper . lines where
+  wrapper arg = "part 1:\n" ++ (show $ part1 arg)
+         ++ "\n\npart 2:\n" ++ (show $ part2 arg)
