62934eea6975184a2b929c9a8e52c0ab31173572
dzwdz <kg67199@gmail.com>
2021-12-03 12:28:30 +0100

    combine day 2 solutions

--- 2.hs
+++ 2.hs
@@ -14,11 +14,23 @@ parse line =
     (cmd, arg) = fromJust $ splitSingle line ' '
     i = readInteger arg
 
-sol = applyToTuple (*) . foldl step (0, 0) . map parse
+part1 = applyToTuple (*) . foldl step (0, 0) . map parse
   where
     step (pos, depth) ("forward", amt) = (pos + amt, depth)
     step (pos, depth) ("down",    amt) = (pos, depth + amt)
     step (pos, depth) ("up",      amt) = (pos, depth - amt)
 
+part2 = (\(p,d,a) -> p * d) . foldl step (0, 0, 0) . map parse
+  where
+    step (pos, depth, aim) ("forward", amt) =
+      (pos + amt, depth + aim * amt, aim)
+    step (pos, depth, aim) ("down",    amt) =
+      (pos, depth, aim + amt)
+    step (pos, depth, aim) ("up",      amt) =
+      (pos, depth, aim - amt)
+
+
 main :: IO ()
-main = interact $ show . sol . lines
+main = interact $ wrapper . lines where
+  wrapper arg = "part 1:\n" ++ (show $ part1 arg)
+         ++ "\n\npart 2:\n" ++ (show $ part2 arg)
