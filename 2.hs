657c284324b7d16b0a4d34e8933eb200f49f723d
dzwdz <kg67199@gmail.com>
2021-12-03 12:36:41 +0100

    clean up day 2

--- 2.hs
+++ 2.hs
@@ -4,8 +4,6 @@ import Data.Maybe
 readInteger :: String -> Integer
 readInteger = read
 
-applyToTuple fn (a, b) = fn a b -- TODO is that in the stdlib?
-
 splitSingle str delim =
   fmap (\x -> splitAt x str) (elemIndex delim str)
 
@@ -14,19 +12,20 @@ parse line =
     (cmd, arg) = fromJust $ splitSingle line ' '
     i = readInteger arg
 
-part1 = applyToTuple (*) . foldl step (0, 0) . map parse
-  where
-    step (pos, depth) ("forward", amt) = (pos + amt, depth)
-    step (pos, depth) ("down",    amt) = (pos, depth + amt)
-    step (pos, depth) ("up",      amt) = (pos, depth - amt)
 
-part2 = (\(p,d,a) -> p * d) . foldl step (0, 0, 0) . map parse
+part1 = uncurry (*) . foldr step (0, 0) . map parse
   where
-    step (pos, depth, aim) ("forward", amt) =
+    step ("forward", amt) (pos, depth) = (pos + amt, depth)
+    step ("down",    amt) (pos, depth) = (pos, depth + amt)
+    step ("up",      amt) (pos, depth) = (pos, depth - amt)
+
+part2 = (\(p,d,a) -> p * d) . foldr step (0, 0, 0) . map parse
+  where
+    step ("forward", amt) (pos, depth, aim) =
       (pos + amt, depth + aim * amt, aim)
-    step (pos, depth, aim) ("down",    amt) =
+    step ("down",    amt) (pos, depth, aim) = 
       (pos, depth, aim + amt)
-    step (pos, depth, aim) ("up",      amt) =
+    step ("up",      amt) (pos, depth, aim) =
       (pos, depth, aim - amt)
 
 
