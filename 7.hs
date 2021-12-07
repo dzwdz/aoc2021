af05ffe6d3720015bf1af7c97315d81c830ab1a8
dzwdz <kg67199@gmail.com>
2021-12-07 10:14:09 +0100

    day 7 part 1

---
+++ 7.hs
@@ -0,0 +1,30 @@
+import Data.List
+import Data.Ord
+import Debug.Trace
+coolTrace a = trace (show a) a
+
+readInteger :: String -> Integer
+readInteger = read
+
+split :: Eq a => a -> [a] -> [[a]]
+split delim arr =
+  case elemIndex delim arr of
+    Nothing  -> [arr]
+    Just idx -> result where
+      (pre, post') = splitAt idx arr
+      post = tail post'
+      result = [pre] ++ split delim post
+
+cost crabs target = sum $ map (\x -> abs $ target - x) crabs
+
+
+preprocess = map readInteger . split ','
+
+part1 crabs = minimum $ map (cost crabs) (nub crabs)
+
+part2 _ = "TODO"
+
+main :: IO ()
+main = interact $ wrapper . preprocess where
+  wrapper arg = "part 1:\n" ++ (show $ part1 arg)
+         ++ "\n\npart 2:\n" ++ (show $ part2 arg)
