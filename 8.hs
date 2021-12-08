947da39bebb492d5f86379f17c6abe91788e0d99
dzwdz <kg67199@gmail.com>
2021-12-08 20:46:29 +0100

    make day 8 slightly less shit

--- 8.hs
+++ 8.hs
@@ -20,10 +20,10 @@ split delim arr =
 preprocess = map processLine . lines where
   processLine line = map (map sort) $ split "|" $ words line
 
-part1 patterns = length $ concat $ map stuff patterns where
+part1 = length . concat . map stuff where
   stuff [_, output] = filter (\x->elem (length x) [2, 3, 4, 7]) output
 
-part2 patterns = sum $ map translate patterns where
+part2 = sum . map translate where
   translate [input, output] = asNumber where
     ofLength len = filter (\a -> length a == len) input
 
@@ -32,19 +32,19 @@ part2 patterns = sum $ map translate patterns where
     d4 = head $ ofLength 4
     d8 = head $ ofLength 7
     d3 = head
-          $ filter (\x -> intersect x d1 == d1) -- d3 
-          $ ofLength 5                          -- d2, d3, d5
+       $ filter (\x -> intersect x d1 == d1) -- d3 
+       $ ofLength 5                          -- d2, d3, d5
     [d2, d5] = sortBy (comparing
                             (length . intersect d4)) -- [d2 (len 2), d5 (len 3)]
-                   $ filter (/= d3) -- d2 ; d5
-                   $ ofLength 5     -- d2 ; d3 ; d5
+             $ filter (/= d3) -- d2 ; d5
+             $ ofLength 5     -- d2 ; d3 ; d5
     d9 = head
-           $ filter (\x -> intersect x d4 == d4)
-           $ ofLength 6
+       $ filter (\x -> intersect x d4 == d4)
+       $ ofLength 6
     [d6, d0] = sortBy (comparing
                (length . intersect d1))
-      $ filter (/= d9)
-      $ ofLength 6
+             $ filter (/= d9)
+             $ ofLength 6
     digitList = [d0, d1, d2, d3, d4, d5, d6, d7, d8, d9]
 
     digits = map (\d -> fromJust $ elemIndex d digitList) output
